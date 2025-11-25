106c87ded79305e1e5ec7b65ac40374b214e74ef
gustavobugeno <cb.bugueno@hotmail.com>
2025-11-25 07:47:09 -0300

    Update settings.py

--- cbc/settings.py
+++ cbc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv 
 import os
@@ -108,12 +108,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cbc.wsgi.application'
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
