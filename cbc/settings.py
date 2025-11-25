6f935984c788d5f4359d9634ec75495dc9b22f52
gustavobugeno <cb.bugueno@hotmail.com>
2025-11-25 06:12:44 -0300

    Update settings.py

--- cbc/settings.py
+++ cbc/settings.py
@@ -65,6 +65,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'csp',
+    'cloudinary',
+    'cloudinary_storage',
+
 ]
 
 MIDDLEWARE = [
@@ -141,8 +144,17 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# CLOUDINARY CONFIG
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': os.getenv("CLOUDINARY_API_KEY"),
+    'API_SECRET': os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # DESACTIVAR SMTP (Render NO permite conexiones SMTP)
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
