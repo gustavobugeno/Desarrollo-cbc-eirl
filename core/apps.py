459d60a29fd552db0bd2bfcc2b4f778526b17414
gustavobugeno <cb.bugueno@hotmail.com>
2025-11-24 23:50:04 -0300

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # Ejecutar el script para crear un superusuario automáticamente
+        import core.create_superuser
