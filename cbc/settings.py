5f7839d2230d7ec906ff531e6ba1be54089185dc
gustavo bugueño <cb.bugueno@hotmail.com>
2026-04-13 14:45:03 -0400

    Update settings.py

--- cbc/settings.py
+++ cbc/settings.py
@@ -13,16 +13,7 @@ import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv 
 import os
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="tuemail@gmail.com",
-        password="admin123"
-    )
 
 load_dotenv()  # carga variables desde .env local
 BASE_DIR = Path(__file__).resolve().parent.parent
